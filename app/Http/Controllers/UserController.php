e08b7b7bbf300cff9d38074ca9cb9911cefe0803
Luiz Bono <lfamlfam@gmail.com>
2021-04-23 22:06:10 -0300

    Fim da aula

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -103,7 +103,32 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,
+                        ['name' => 'required',
+                        'email' => 'required|email|unique:users,email',
+                        'password' => 'required|same:confirm-password',
+                        'roles' => 'required']);
+
+        $input = $request->all();
+
+        if(!empty($input['password'])){
+
+            $input['password'] = Hash::make($input['password']);
+
+        }else{
+
+            $input = Arr::except( $input, ['password']);
+        }
+
+        $user = User::find($id);
+
+        $user->update($input);
+
+        DB::table('model_has_roles')->where('model_id',$id)->delete();
+
+        $user->assignRole($request->input('roles'));
+
+        return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso');
     }
 
     /**
